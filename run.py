fc51751288124b622c1e2657766d763ed9b09c5a
Bora Toprak Temir <127056351+ToprakTemir@users.noreply.github.com>
2025-05-23 14:56:53 +0300

    deleted unused css folder

--- run.py
+++ run.py
@@ -10,9 +10,13 @@ app.secret_key = '@p1l2jckk-_12x-uszu)d%q##4x3n2hukp5(t8q)+1_o^d5mao'
 if __name__ == '__main__':
 
     # execute seed_data() to refresh the database with initial data
-    if (len(sys.argv) > 1 and sys.argv[1] == 'initialize'):
-        seed_data()
-        execute_sql_file('app/sql/procedures.sql')
-        execute_sql_file('app/sql/triggers.sql')
+    # if (len(sys.argv) > 1 and sys.argv[1] == 'initialize'):
+    #     seed_data()
+    #     execute_sql_file('app/sql/procedures.sql')
+    #     execute_sql_file('app/sql/triggers.sql')
+
+    seed_data()
+    execute_sql_file('app/sql/procedures.sql')
+    execute_sql_file('app/sql/triggers.sql')
 
     app.run(debug=True)
